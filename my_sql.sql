4a0aa14f7aba32a89cac34068c0849f1a776752a
rohitbarha <rohitbarha@gmail.com>
2021-07-09 14:56:25 +0530

    Create my_sql.sql

---
+++ my_sql.sql
@@ -0,0 +1,16 @@
+-select rows from table-
+
+SELECT * FROM mytable
+
+
+-update table-
+
+UPDATE mytable
+SET category = "Coke" 
+WHERE meal_id = 2215;
+
+-delete from table-
+
+DELETE FROM mytable
+WHERE meal_id = 1571;
+
